4cb289b001c2afd113fb5eb733ead859028756b6
Grzegorz Bielański <gbielanski@poczta.onet.pl>
2019-11-24 23:08:36 +0100

    steps

---
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -0,0 +1,25 @@
+package com.example.android.dagger.di
+
+import android.content.Context
+import com.example.android.dagger.main.MainActivity
+import com.example.android.dagger.registration.RegistrationActivity
+import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
+import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [StorageModule::class])
+interface AppComponent{
+
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance context: Context): AppComponent
+    }
+
+    fun inject (activity: RegistrationActivity)
+    fun inject (activity: MainActivity)
+    fun inject (fragment: EnterDetailsFragment)
+    fun inject (fragment: TermsAndConditionsFragment)
+}
